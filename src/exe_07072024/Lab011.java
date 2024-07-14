18af1ca3896eed2063d568815bf193a840dd8cdc
manojkarra <manojkarra123@gmail.com>
2024-07-14 16:05:13 +0530

    new files

---
+++ src/exe_07072024/Lab011.java
@@ -0,0 +1,11 @@
+package exe_07072024;
+
+public class Lab011
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Manoj");
+        //No ln in printing statments
+        System.out.print("No spacing");
+    }
+}
