1ec10f3f55699410731df1658fb079a23344401b
manojkarra <manojkarra123@gmail.com>
2024-07-14 16:24:13 +0530

    new files adding latest

--- src/exe_07072024/Lab014.java
+++ src/exe_07072024/Lab014.java
@@ -2,6 +2,7 @@ package exe_07072024;
 
 public class Lab014 {
     public static void main(String[] args) {
-    //public static void main(String args[]) --> This can be used as well
+        //public static void main(String args[]) --> This can be used as well
         System.out.println("Yes, can be used in both ways");
+    }
 }
