12142915eb7de4240216e25e7debe0deb247465e
manojkarra <manojkarra123@gmail.com>
2024-07-16 00:30:34 +0530

    130724 files are committed

---
+++ src/exe_13072024/Lab024.java
@@ -0,0 +1,8 @@
+package exe_13072024;
+
+public class Lab024 {
+    public static void main(String[] args) {
+        int n=9;
+        System.out.printf("%d",n);
+    }
+}
---
+++ src/exe_13072024/Lab041.java
@@ -0,0 +1,17 @@
+package exe_13072024;
+
+public class Lab041 {
+    public static void main(String[] args) {
+        // Compound Assignment Operators
+        int a = 10;
+        // += , -=, /=, *= . %=
+        // += ->
+        a += 10; // a = a+10
+        a -= 10; // a = a-10
+        a /= 10; // a = a/10
+        a *= 10; // a = a*10
+        System.out.println(a+=10);
+
+
+    }
+}
